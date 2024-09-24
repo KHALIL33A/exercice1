3d48461d0ded139d93a75093bc42e555ecafbb2d
KHALIL33A <ayoub652001@gmail.com>
2024-09-24 15:12:35 +0200

    khalid.c

---
+++ khalid.c
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+ char nom[] = "khalil";
+ char prenom [] ="ayoub";
+ printf("my nam is: %s \n",nom);
+  printf("my first name is: %s \n",prenom);
+  
+ char sex [] = "homme";
+ printf("my sex is : %s \n",sex);
+ 
+ char email [] = "ayoubart65@gmail.com";
+ printf("my email : %s \n",email);
+ int age = 23;
+ printf("my age is : %d \n",age);
+ 
+ return 0;
+
+}
