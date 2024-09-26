5e99424409e105d9d835a711b84c154681769a35
KHALIL33A <ayoub652001@gmail.com>
2024-09-26 17:26:29 +0200

    newboucle

---
+++ E boucle 3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+ int x , y=0 ,i  ;
+ printf("veuillez vous entre le nomber x :");
+ scanf ("%d",&x);
+ for( i=1 ; i<= x ; i++){
+    y+=i ;
+ }
+     printf("%d" ,y);
+     
+ 
+    
+    return 0;
+    }
