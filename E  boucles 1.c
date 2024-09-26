f88d180f3b3fd23b655cfbd23e50e41150021837
KHALIL33A <ayoub652001@gmail.com>
2024-09-26 15:17:07 +0200

    new E boucle

---
+++ E  boucles 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+ int x, a =1;
+ printf("veuillez entre le nomber x :");
+ scanf("%d",&x);
+ while (a<=10){
+     printf("%d * %d = %d \n",x,a,x*a);
+     a+=1
+     ;
+ }
+
+
+  
+    return 0;
+    }
