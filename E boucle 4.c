33138fd90cbcc60f47d2efe2142f442e2ce4cc5f
KHALIL33A <ayoub652001@gmail.com>
2024-09-26 18:00:08 +0200

    new boucle

---
+++ E boucle 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+int N , i ;
+printf ("veuillez vous entre le nomber N :");
+scanf("%d",&N);
+for (i=0 ; i < N ;i++){
+    printf("%d \n", 2*i +1);
+}
+
+ 
+    
+    return 0;
+    }
