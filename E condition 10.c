3b346729c4ddbc05f06b33b6638caa9681bd2a7b
KHALIL33A <ayoub652001@gmail.com>
2024-09-26 11:33:28 +0200

    condition 10

---
+++ E condition 10.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main() {
+int j ,m ,a ;
+printf("veuilles vous entre jour j :");
+scanf("%d",&j);
+printf("veuilles vous entre moin m :");
+scanf("%d",&m);
+printf("veuilles vous entre a :");
+scanf("%d",&a);
+ switch (m){
+     
+ case 1:
+ printf ("%d / janvier/ %d", j,a);
+ break;
+ case 2:
+ printf ("%d / février / %d", j,a);
+ break;
+ case 3:
+ printf ("%d / Mars / %d", j,a);
+ break;
+ case 4:
+ printf (" %d / avril/ %d", j,a);
+ break;
+  case 5:
+ printf ("%d / mai / %d", j,a);
+ break;
+  case 6:
+ printf (" %d / juin / %d", j,a);
+ break;
+  case 7:
+ printf ("%d / juitte / %d", j,a);
+ break;
+  case 8:
+ printf ("%d / aout / %d", j,a );
+ break;
+ case 9:
+ printf ("%d /septembre / %d", j,a);
+ break;
+ case 10:
+ printf ("%d /  octobre / %d", j,a);
+ break;
+ case 11:
+ printf ("%d /  novembre / %d", j,a);
+ break;
+ case 12:
+ printf ("%d / décembre / %d", j,a);
+ break;
+  
+ 
+ default:
+ 
+ }
+
+
+    
+    
+
+   
+  
+    return 0;
+    }
