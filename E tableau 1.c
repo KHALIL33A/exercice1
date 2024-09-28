06513699ed00cd41de7c4e119ed063df87919f2d
KHALIL33A <ayoub652001@gmail.com>
2024-09-28 11:24:20 +0200

    tableau

---
+++ E tableau 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   
+    int T[] = {1, 2, 3, 4, 5}, i;
+    for ( i=0 ;i<5;i++){
+        printf("fonction :%d = %d \n",i ,T[i]);
+    }
+    
+    
+    
+    
+    return 0;
+        
+    }
