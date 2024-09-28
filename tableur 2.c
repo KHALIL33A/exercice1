7fad8d826934668f89290425d57669c99f8fe2ee
KHALIL33A <ayoub652001@gmail.com>
2024-09-28 14:53:17 +0200

    tableau

---
+++ tableur 2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+   
+   int   n , i = 0;
+   printf("veuillez vous entre le nomber :\n");
+   //scanf("%d",&n);
+   
+   int tableua [5] ;
+   for (i=0; i<5 ;i++){
+       printf("tableua [%d]= ",i);
+       scanf("%d",&tableua [i]);
+   }
+   
+   
+    
+    return 0;
+        
+    }
