d5b32ca3e7f6482600fdc0f862776249efde90d2
KHALIL33A <ayoub652001@gmail.com>
2024-10-01 15:04:02 +0200

    E fonction 4

---
+++ E fonction 4.c
@@ -0,0 +1,20 @@
+int nomber(int x ,int y){
+    
+   return x,y;
+  
+}
+int main(){
+   int  x , y;
+   int minimon ;
+   printf("entre le nomber x: ");
+   scanf("%d",&x);
+   printf("entre le nomber y: ");
+   scanf("%d",&y);
+   if(x<y){
+       printf("le nomber ete minimom X : %d",x);
+   }else if(y<x){
+       printf("le nomber et minimin Y :%d",y);
+   }
+  
+    return 0; 
+}
