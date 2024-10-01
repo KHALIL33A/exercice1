fc70813d1afeb1e6e23470a4fd9bff4b1ed17491
KHALIL33A <ayoub652001@gmail.com>
2024-10-01 12:28:44 +0200

    E fonction 2

---
+++ E fonction 2.c
@@ -0,0 +1,16 @@
+int multuply(int x ,int y){
+    
+   return x*y;
+  
+}
+int main(){
+   int  x , y;
+   int resulte;
+   printf("entre le nomber: ");
+   scanf("%d",&x);
+   printf("entre le nomber: ");
+   scanf("%d",&y);
+   resulte=multuply(x,y);
+  printf("resultat =%d ",resulte );
+    return 0; 
+}
